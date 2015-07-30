b37cf8357b4d05ffdb82aa98830ce980856a2e91
John Leszczynski <jrleszczynski@gmail.com>
2015-07-30 17:01:21 -0400

    refactor: hand_rank

--- poker/poker.py
+++ poker/poker.py
@@ -16,64 +16,31 @@ def allmax(iterable, key=None):
     return result
 
 
-def card_ranks(hand):
-    "Return a list of the ranks, sorted with higher first."
-    ranks = ['--23456789TJQKA'.index(r) for r,s in hand]
-    ranks.sort(reverse=True)
-    if ranks == [14, 5, 4, 3, 2]:
-        ranks = [5, 4, 3, 2, 1] # straight low ace special case
-    return ranks
-
-
-def straight(ranks):
-    "Return True if the ordered ranks from a 5-card straight."
-    return ranks == range(ranks[0], ranks[0]-5, -1)
-
-
-def flush(hand):
-    "Return True if all the cards have the same suit."
-    suits = [s for r,s in hand]
-    return len(set(suits)) == 1
-
-
-def kind(n, ranks):
-    """Return the first rank that this hand has exactly n of.
-    Return None if there is no n-of-a-kind in the hand."""
-    for r in set(ranks):
-        if ranks.count(r) == n: return r
-    return None
-
-def two_pair(ranks):
-    """If there are two pair, return the two ranks as a
-    tuple: (highest, lowest); otherwise return None."""
-    first = kind(2, ranks)
-    second = kind(2, ranks[::-1])
-    if first and first != second:
-        return (first, second)
-    return None
-
-
 def hand_rank(hand):
     "Return a value indicating the ranking of a hand."
-    ranks = card_ranks(hand)
-    if straight(ranks) and flush(hand):            # straight flush
-        return (8, max(ranks))
-    elif kind(4, ranks):                           # 4 of a kind
-        return (7, kind(4, ranks), kind(1, ranks))
-    elif kind(3, ranks) and kind(2, ranks):        # full house
-        return (6, kind(3, ranks), kind(2, ranks))
-    elif flush(hand):                              # flush
-        return (5, ranks)
-    elif straight(ranks):                          # straight
-        return (4, max(ranks))
-    elif kind(3, ranks):                           # 3 of a kind
-        return (3, kind(3, ranks), ranks)
-    elif two_pair(ranks):                          # 2 pair
-        return (2, two_pair(ranks), ranks)
-    elif kind(2, ranks):                           # kind
-        return (1, kind(2, ranks), ranks)
-    else:                                          # high card
-        return (0, ranks)
+    # counts is the count of each rank; ranks lists corresponding ranks
+    groups = group(['--23456789TJQKA'.index(r) for r,s in hand])
+    counts, ranks = zip(*groups)
+    if ranks == (14, 5, 4, 3, 2):
+        ranks = (5, 4, 3, 2, 1) # straight with low Ace
+    straight = len(ranks) == 5 and max(ranks)-min(ranks) == 4
+    flush = len(set([s for r,s in hand])) == 1
+    return (9 if (5,) == counts else               # 5 of a kind
+            8 if straight and flush else           # straight flush
+            7 if (4, 1) == counts else             # 4 of a kind
+            6 if (3, 2) == counts else             # full house
+            5 if flush else                        # flush
+            4 if straight else                     # straight
+            3 if (3, 1, 1) == counts else          # 3 of a kind
+            2 if (2, 2, 1) == counts else          # 2 pair
+            1 if (2, 1, 1, 1) == counts else       # 2 of a kind
+            0), ranks
+
+def group(items):
+    "Return a list of [(count, x)...], highest count first, then highest x first."
+    groups = [(items.count(item), item) for item in set(items)]
+    return sorted(groups, reverse=True)       
+
 
 def test():
     "Test cases for the functions in poker program."
@@ -81,27 +48,11 @@ def test():
     fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
     fh = "TD TC TH 7C 7D".split() # Full House
     tp = "5S 5D 9H 9C 6S".split() # Two pairs
-    fkranks = card_ranks(fk)
-    tpranks = card_ranks(tp)
-    assert kind(4, fkranks) == 9
-    assert kind(3, fkranks) == None
-    assert kind(2, fkranks) == None
-    assert kind(1, fkranks) == 7
-    assert card_ranks(sf) == [10, 9, 8, 7, 6]
-    assert card_ranks(fk) == [9, 9, 9, 9, 7]
-    assert card_ranks(fh) == [10, 10, 10, 7, 7]
-    assert straight([9, 8, 7, 6, 5]) == True
-    assert straight([9, 8, 8, 6, 5]) == False
-    assert flush(sf) == True
-    assert flush(fk) == False
     assert poker([sf, fk, fh]) == [sf]
     assert poker([fh, fk]) == [fk]
     assert poker([fh, fh]) == [fh, fh]
     assert poker([fh]) == [fh]
     assert poker([sf] + 99*[fh]) == [sf]
-    assert hand_rank(sf) == (8, 10)
-    assert hand_rank(fk) == (7, 9, 7)
-    assert hand_rank(fh) == (6, 10, 7)
     return "tests pass"
 
 print test()
