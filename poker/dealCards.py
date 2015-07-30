e7ba34ca0e1c77cfcb5f4b8d68f7e46bce1d7844
John Leszczynski <jrleszczynski@gmail.com>
2015-07-30 16:18:58 -0400

    fix: typo in original deck creation

--- poker/dealCards.py
+++ poker/dealCards.py
@@ -1,6 +1,6 @@
 import random
 
-def deal(numhands, n=5, deck=[r+s for r in '23456789TJQKS' for s in 'SHDC']):
+def deal(numhands, n=5, deck=[r+s for r in '23456789TJQKA' for s in 'SHDC']):
     "Shuffle the deck and deal out numhands n-card hands."
     random.shuffle(deck)
     return [deck[hand:hand+n] for hand in range(0, n*numhands, n)]
