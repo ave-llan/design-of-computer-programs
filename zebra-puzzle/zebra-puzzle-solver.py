f7a7a8d469afd871187cb64fc88681834cd659d0
John Leszczynski <jrleszczynski@gmail.com>
2015-08-10 11:08:25 -0400

    initial commit of solver

---
+++ zebra-puzzle/zebra-puzzle-solver.py
@@ -0,0 +1,14 @@
+from itertools import permutations
+
+houses = [1, 2, 3, 4, 5]
+orderings = list(permutations(houses))
+
+for (red, green, ivory, yellow blue) in orderings:
+    for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in orderings:
+        for (dog, snails, fox, horse, ZEBRA) in orderings:
+            for (coffee, tea, milk, oj, WATER) in orderings:
+                for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings:
+                    # Check if all conditions are met
+                    pass
+
+
