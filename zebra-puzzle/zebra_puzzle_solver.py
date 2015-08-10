c988a718950a99d7a13cb06e279721425d39d8c6
John Leszczynski <jrleszczynski@gmail.com>
2015-08-10 11:46:05 -0400

    feat: optimize solution by checking for conditinos sooner

--- zebra-puzzle/zebra_puzzle_solver.py
+++ zebra-puzzle/zebra_puzzle_solver.py
@@ -15,23 +15,23 @@ def zebra_puzzle():
     orderings = list(permutations(houses))
     return next((WATER, ZEBRA)
             for (red, green, ivory, yellow, blue) in orderings
+            if imright(green, ivory)        #6
             for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in orderings
-            for (dog, snails, fox, horse, ZEBRA) in orderings
-            for (coffee, tea, milk, oj, WATER) in orderings
-            for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
             if Englishman is red            #2
-            if Spaniard is dog              #3
+            if Norwegian is first           #10
+            if nextto(Norwegian, blue)      #15
+            for (coffee, tea, milk, oj, WATER) in orderings
             if coffee is green              #4
             if Ukranian is tea              #5
-            if imright(green, ivory)        #6
-            if OldGold is snails            #7
-            if Kools is yellow              #8
             if milk is middle               #9
-            if Norwegian is first           #10
-            if nextto(Chesterfields, fox)   #11
-            if nextto(Kools, horse)         #12
+            for (OldGold, Kools, Chesterfields, LuckyStrike, Parliaments) in orderings
+            if Kools is yellow              #8
             if LuckyStrike is oj            #13
             if Japanese is Parliaments      #14
-            if nextto(Norwegian, blue)      #15
+            for (dog, snails, fox, horse, ZEBRA) in orderings
+            if Spaniard is dog              #3
+            if OldGold is snails            #7
+            if nextto(Chesterfields, fox)   #11
+            if nextto(Kools, horse)         #12
             )
 
