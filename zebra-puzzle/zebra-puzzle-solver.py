1e8506ff968d6ec81f4f2f97956d3ca83ece8b0f
John Leszczynski <jrleszczynski@gmail.com>
2015-08-10 11:13:26 -0400

    add helper functions to check for positional relations

--- zebra-puzzle/zebra-puzzle-solver.py
+++ zebra-puzzle/zebra-puzzle-solver.py
@@ -3,6 +3,16 @@ from itertools import permutations
 houses = [1, 2, 3, 4, 5]
 orderings = list(permutations(houses))
 
+
+def imright(h1, h2):
+    "House h1 is immediately right of h2 if h1-h2 == 1."
+    return h1-h2 == 1
+
+def nextto(h1, h2):
+    "Two houses are next to each other if they differ by 1."
+    return abs(h1-h2) == 1
+
+
 for (red, green, ivory, yellow blue) in orderings:
     for (Englishman, Spaniard, Ukranian, Japanese, Norwegian) in orderings:
         for (dog, snails, fox, horse, ZEBRA) in orderings:
