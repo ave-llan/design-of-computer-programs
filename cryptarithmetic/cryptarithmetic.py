74bca78618fd91ba6b40d8a73f16d5ad857519ac
John Leszczynski <jrleszczynski@gmail.com>
2015-08-10 15:52:55 -0400

    add cryptarithemtic solver

---
+++ cryptarithmetic/cryptarithmetic.py
@@ -0,0 +1,27 @@
+import string, re
+from itertools import permutations
+
+
+def solve(formula):
+    """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
+    Input formula is a string; output is a digit-filled-in string or None."""
+    for f in fill_in(formula):
+        if valid(f):
+            return f
+   
+
+def fill_in(formula):
+    "Generate all possible fillings-in of letters in formula with digits."
+    letters = ''.join(set(re.findall(r'[A-Z]', formula)))
+    for digits in permutations('1234567890', len(letters)):
+        table = string.maketrans(letters, ''.join(digits))
+        yield formula.translate(table)
+
+    
+def valid(f):
+    """Formula f is valid iff it has no numbers with 
+    leading zero and evals to True."""
+    try:
+        return not re.search(r'\b0[0-9]', f) and eval(f) is True
+    except ArithmeticError:
+        return False
