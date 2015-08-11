09dd8d5f3b61ffb47e1c63f0ffbef151cc3eb249
John Leszczynski <jrleszczynski@gmail.com>
2015-08-11 09:45:57 -0400

    optimze cryptarithemtic

--- cryptarithmetic/cryptarithmetic.py
+++ cryptarithmetic/cryptarithmetic.py
@@ -8,7 +8,7 @@ def solve(formula):
     Input formula is a string; output is a digit-filled-in string or None."""
     for f in fill_in(formula):
         if valid(f):
-            return f
+            print f
    
 
 def fill_in(formula):
@@ -26,3 +26,48 @@ def valid(f):
         return not re.search(r'\b0[0-9]', f) and eval(f) is True
     except ArithmeticError:
         return False
+
+
+def compile_word(word):
+    """Compile a word of uppercase letters as numeric digits.
+    E.g., compile_word('YOU') => '(1*U+10*O+100*Y)'
+    Non-uppercase words unchanged: compile_word('+') => '+'"""
+    if word.isupper():
+        terms = ['{}*{}'.format(10**i, letter)
+                    for (i, letter) in enumerate(word[::-1])]
+        return '(' + '+'.join(terms) + ')'
+    else:
+        return word
+
+
+def compile_formula(formula, verbose=False):
+    """Compile formula into a function. Also return letters found, as a str,
+    in same order as parms of function. The first digit of a multi-digit 
+    number can't be 0. So if YOU is a word in the formula, and the function
+    is called with Y eqal to 0, the function should return False."""
+    
+    # modify the code in this function.
+    
+    letters = ''.join(set(re.findall('[A-Z]', formula)))
+    parms = ', '.join(letters)
+    tokens = map(compile_word, re.split('([A-Z]+)', formula))
+    body = ' and '.join([word[0] + ' != 0' for word in set(re.findall(r'[A-Z]+', formula))])
+    body += ' and ' + ''.join(tokens)
+    f = 'lambda %s: %s' % (parms, body)
+    if verbose: print f
+    return eval(f), letters
+
+
+def faster_solve(formula):
+    """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
+    Input formula is a string; output is a digit-filled-in string or None.
+    This version precompiles the formula; only one eval per formula."""
+    f, letters = compile_formula(formula)
+    for digits in itertools.permutations((1,2,3,4,5,6,7,8,9,0), len(letters)):
+        try:
+            if f(*digits) is True:
+                table = string.maketrans(letters, ''.join(map(str, digits)))
+                return formula.translate(table)
+        except ArithmeticError:
+            pass
+
