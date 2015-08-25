5709a16968c4c3e2ed6177428396bfc6945e98b6
John Leszczynski <jrleszczynski@gmail.com>
2015-08-25 17:14:41 -0400

    add decorator

--- regular-expressions/generator.py
+++ regular-expressions/generator.py
@@ -1,11 +1,18 @@
 def lit(s):
     set_s = set([s])
     return lambda Ns: set_s if len(s) in Ns else null
+
 def alt(x, y):      return lambda Ns: x(Ns) | y(Ns)
+
 def star(x):        return lambda Ns: opt(plus(x))(Ns)
+
 def plus(x):        return lambda Ns: genseq(x, star(x), Ns, startx=1) #Tricky
+
 def oneof(chars):   return lambda Ns: set(chars) if 1 in Ns else null
+
+@n_ary
 def seq(x, y):      return lambda Ns: genseq(x, y, Ns)
+
 def opt(x):         return alt(epsilon, x)
 dot = oneof('?')    # You could expand the alphabet to more chars.
 epsilon = lit('')   # The pattern that matches the empty string.
@@ -17,8 +24,6 @@ def genseq(x, y, Ns, startx=0):
     if not Ns:
         return null
     xmatches = x(set(range(startx, max(Ns)+1)))
-    print 'xmatches:'
-    print xmatches
     Ns_x = set(len(m) for m in xmatches)
     Ns_y = set(n-m for n in Ns for m in Ns_x if n-m >= 0)
     ymatches = y(Ns_y)
@@ -26,12 +31,22 @@ def genseq(x, y, Ns, startx=0):
                for m1 in xmatches for m2 in ymatches
                if len(m1+m2) in Ns)
 
+from functools import update_wrapper
 
+def decorator(d):
+    "Make function d a decorator: d wraps a function fn."
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+    update_wrapper(_d, d)
+    return _d
+
+@decorator
 def n_ary(f):
     """Given binary function f(x, y), return an n_ary function such
     that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
     def n_ary_f(x, *args):
         return x if not args else f(x, n_ary_f(*args))
+    update_wrapper(n_ary_f, f) # add documentation from original function
     return n_ary_f
 
 
