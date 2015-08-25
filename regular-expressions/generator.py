c40a8de89f2e6cf28e697771a79a1fc42d43cb01
John Leszczynski <jrleszczynski@gmail.com>
2015-08-25 17:02:15 -0400

    add n_ary function builder

--- regular-expressions/generator.py
+++ regular-expressions/generator.py
@@ -27,6 +27,14 @@ def genseq(x, y, Ns, startx=0):
                if len(m1+m2) in Ns)
 
 
+def n_ary(f):
+    """Given binary function f(x, y), return an n_ary function such
+    that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
+    def n_ary_f(x, *args):
+        return x if not args else f(x, n_ary_f(*args))
+    return n_ary_f
+
+
 def test():
 
     f = lit('hello')
