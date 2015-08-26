9492a4a4a595b5477cd361ae50f5e1a35dab6f91
John Leszczynski <jrleszczynski@gmail.com>
2015-08-26 07:30:41 -0400

    add tracer

--- regular-expressions/decorators.py
+++ regular-expressions/decorators.py
@@ -43,6 +43,33 @@ def countcalls(f):
 
 callcounts = {}
 
-@countcalls
-def fib(n): return 1 if n <= 1 else fib(n-1) + fib(n-2)
+#BUGBUG returning None
+@decorator
+def trace(f):
+    indent = '   '
+    def _f(*args):
+        signature = '%s(%s)' % (f.__name__, ', '.join(map(repr, args)))
+        print '%s--> %s' % (trace.level*indent, signature)
+        trace.level += 1
+        try:
+            result = f(*args)
+            print '%s<-- %s == %s' % ((trace.level-1)*indent,
+                                      signature, result)
+        finally:
+            trace.level -= 1
+        return # your code here
+    trace.level = 0
+    return _f
 
+
+@trace
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+@trace
+def gcd(a, b):
+    if b > 0:
+        return gcd(b, a%b)
+    return a
