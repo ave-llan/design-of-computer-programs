605353247b6e7b0a87e55bb946adf5c8bf596363
John Leszczynski <jrleszczynski@gmail.com>
2015-08-26 07:38:35 -0400

    allow whitespace

--- regular-expressions/grammar.py
+++ regular-expressions/grammar.py
@@ -8,9 +8,20 @@ Exps    => Exp [,] Exps | Exp
 Var     => [a-zA-Z_]\w*
 Num     => [-+]?[0-9]+([.][0-9]*)?"""
 
-def grammar(description):
-  """Convert a description to a grammar. """
-  G = {}
+def grammar(description, whitespace=r'\s*'):
+  """Convert a description to a grammar. Each line is a rule for a
+  non-terminal symbol; it looks like this:
+    Symbol =>  A1 A2 ... | B1 B2 ... | C1 C2 ...
+  where the right-hand side is one or more alternatives, separated by
+  the '|' sign. Each alternative is a sequence of atoms, separated by
+  spaces. An atom is either a symbol on some left-hand side, or it is
+  a regular expression that will be passed to re.match to match a token.
+
+  The grammar that gets defined allows whitespace between tokens by default
+  specify '' as the second argument to grammar() to disallow this (or supply
+  any regular expression to describe allowable whitespace between tokens)."""
+  G = {' ': whitespace}
+  description = description.replace('\t', ' ') # remove all tabs
   for line in split(description, '\n'):
     print 'current line:'
     print line
