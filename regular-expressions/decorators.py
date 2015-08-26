7ed4f9bbe06d951264aed71b11a02a1c2e1bf771
John Leszczynski <jrleszczynski@gmail.com>
2015-08-26 06:30:47 -0400

    add decorator tests with memoization

---
+++ regular-expressions/decorators.py
@@ -0,0 +1,48 @@
+from functools import update_wrapper
+
+def decorator(d):
+    "Make function d a decorator: d wraps a function fn."
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+    update_wrapper(_d, d)
+    return _d
+
+@decorator
+def n_ary(f):
+    """Given binary function f(x, y), return an n_ary function such
+    that f(x, y, z) = f(x, f(y,z)), etc. Also allow f(x) = x."""
+    def n_ary_f(x, *args):
+        return x if not args else f(x, n_ary_f(*args))
+    update_wrapper(n_ary_f, f) # add documentation from original function
+    return n_ary_f
+
+@decorator
+def memo(f):
+    """Decorator that caches the return value for each call to f(args).
+    Then when called again wtih same args, we can just look it up."""
+    cache = {}
+    def _f(*args):
+        try:
+            return cache[args]
+        except KeyError:
+            cache[args] = result = f(*args)
+            return result
+        except TypeError:
+            # some element of args can't be a dict key
+            return f(args)
+    return _f
+
+@decorator
+def countcalls(f):
+    "Decorator that makes the function count calls to it, in callcounts[f]."
+    def _f(*args):
+        callcounts[_f] += 1
+        return f(*args)
+    callcounts[_f] = 0
+    return _f
+
+callcounts = {}
+
+@countcalls
+def fib(n): return 1 if n <= 1 else fib(n-1) + fib(n-2)
+
