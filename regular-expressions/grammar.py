1b353252aacef1043d537bcc6a163972beba5182
John Leszczynski <jrleszczynski@gmail.com>
2015-08-26 07:58:15 -0400

    add parser

--- regular-expressions/grammar.py
+++ regular-expressions/grammar.py
@@ -34,3 +34,37 @@ G = grammar(defined)
 
 print G
 
+def parse(start_symbol, text, grammar):
+  """Example call: parse('Exp', '3*x + b', G).
+  Returns a (tree, remainder) pair. If remainder is '',
+  it has parsed the whole string. Failure iff remainder is None.
+  This is a deterministic PEG parser, so rule order (left-to-right)
+  matters. Do 'E => T op E | T', putting the longest parse first;
+  don't do 'E => T | T op E'
+  Also, no left recursion allowed: don't do 'E => E op T' """
+
+  tokenizer = grammar[' '] + '(%s)'
+
+  def parse_sequence(sequence, text):
+    result = []
+    for atom in sequence:
+      tree, text = parse_atom(atom, text)
+      if text is None: return Fail
+      result.append(tree)
+    return result, text
+
+  def parse_atom(atom, text):
+    if atom in grammar: # Non-Terminal: tuple of alternatives
+      for alternative in grammar[atom]:
+        tree, rem = parse_sequence(alternative, text)
+        if rem is not None: return [atom]+tree, rem
+      return Fail
+    else: # Terminal: match characters against start of text
+      m = re.match(tokenizer % atom, text)
+      return Fail if (not m) else (m.group(1), text[m.end():])
+
+  # Body of parse:
+  return parse_atom(start_symbol, text)
+
+Fail = (None, None)
+
